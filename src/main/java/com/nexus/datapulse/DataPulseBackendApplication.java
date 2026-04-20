4b7a3729bac6b5d49cd877a2890d08c713b36680
danyel513 <daniel.ben.cotoc@gmail.com>
2026-04-20 18:51:00 +0300

    initial commit

---
+++ src/main/java/com/nexus/datapulse/DataPulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.datapulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPulseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataPulseBackendApplication.class, args);
+    }
+
+}
